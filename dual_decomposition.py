4a9b4ac304c4a528496681ccc2588440de96fb39
Neena Parikh <nsparikh@nsparikh-macbookair.roam.corp.google.com>
2015-07-03 12:16:12 -0400

    Method definitions for solver and tester

---
+++ dual_decomposition.py
@@ -0,0 +1,20 @@
+'''
+Minimizes the energy in the pairwise graph.
+Takes inputs:
+	- vector containing number of possible states for each node
+	- vector theta_p, which is real-valued and has length n (where n
+	  is the sum of the number of possible states per node. This vector
+	  contains all the unary potential function values
+	- sparse matrix theta_pq, which is real-valued and size nxn. This
+	  matrix contains all the pairwise potential function values
+'''
+def solver(num_possible_states, theta_p, theta_pq):
+	pass
+
+'''
+Runs the solver by generating a random graph with random potentials,
+finds the assignment with the lowest possible energy, and prints out
+this energy value.
+'''
+def tester():
+	pass
